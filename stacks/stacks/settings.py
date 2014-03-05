37ffa320d769a11a9f9e9f7abbddbcbf286a9c64
Andrew Clarkson <andrewclarkson@live.com>
2014-03-04 19:51:49 -0600

    update

--- stacks/stacks/settings.py
+++ stacks/stacks/settings.py
@@ -57,8 +57,12 @@ WSGI_APPLICATION = 'stacks.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME': 'stacks',
+        'USER': 'Aclark0931',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
